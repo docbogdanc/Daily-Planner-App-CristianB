7d3aa2b2f735ac8495b57d603e61f7799d1fb7a6
docbogdanc <docbogdanc@yahoo.com>
2024-01-21 10:30:59 +0000

    red for current time slot, green for following time slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,30 @@ var blockDays = $('.table');
 // display current date at top page 
 dateElem.text(currentDate);
 
-// change color of row equal with current time
+// change color of row equal with current time and following time slots
 // Loop through each child element
 $('.firstcolumn').each(function() {
-    // go through all text value of the table' times
+    // go through all text value of the table' times from first column
     var childText = $(this).text().trim();
 
     // Check if the time table' time text is equal to the current real time
     if (childText === currentTime) {
-      // return the ID of the table row equivlent to current time
-      var parentID = $(this).parent().attr('id');
-    //   change the background of the row to red to mark current time
-      $('#'+parentID).children().eq(1).removeClass('secondcolumn').addClass('secondcolumnRed');
-    }
-  });
+      // return the ID of the table row equivalent to the current time
+      var currentParentID = $(this).parent().attr('id');
 
+      // change the background of the row to red to mark current time (red row)
+      $('#'+currentParentID).children().eq(1).removeClass('secondcolumn').addClass('secondcolumnRed');
+    
+      // calculate how many hours slot are after current one (red row)
+      var numberOfSiblingsFollowingRed = $('#'+currentParentID).nextAll().length;  
+      console.log(numberOfSiblingsFollowingRed);
+      
+      // loop through all siblings following the red row
+      for (i=0 ; i<numberOfSiblingsFollowingRed ; i++) {
+        // target each sibling second child and change the background to green
+        $('#'+currentParentID).nextAll().eq(i).children().eq(1).removeClass('secondcolumn').addClass('secondcolumnGreen');
+      }
+
+    };
+
+})
